29e9bfbceb7e2139e2ebb7354c9722489b52867e
Faustino Ntemo  Kialungila <faustinontemokialungila@MacBook-Pro-de-Faustino.local>
2017-07-27 11:37:53 +0200

    heroku

--- routes/router.js
+++ routes/router.js
@@ -6,11 +6,11 @@ const requireAuth      = passport.authenticate('jwt', {session: false })
 const requireSignIn    = passport.authenticate('local', {session: false })
 
 module.exports = function(app){
-    app.get('/home', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/build/'));
-    })
+    // app.get('/home', (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'client/build/'));
+    // })
     app.get('/',  (req, res) =>{
-        res.sendFile(path.join(__dirname, '../../client/build/'));
+        res.sendFile(path.join(__dirname, 'client/build/'));
         // res.send({ message: 'Hi, if u out here, is cuz u are auth'}) 
     })
     app.get('/api/users', db.getAllUsers)
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(morgan('combined'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../client/build/')))
+app.use(express.static(path.join(__dirname, 'client/build/')))
 router(app)
 
 
