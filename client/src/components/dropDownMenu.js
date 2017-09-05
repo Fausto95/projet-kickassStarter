48f5708a71b5ac1d5fd39ee855a9192a02b825bd
Faustino Ntemo  Kialungila <faustinontemokialungila@MacBook-Pro-de-Faustino.local>
2017-09-05 10:37:27 +0200

    changed de menu icon size and the link color

--- client/src/components/dropDownMenu.js
+++ client/src/components/dropDownMenu.js
@@ -11,10 +11,10 @@ const DropDownMenu = props => (
             <span className='DropText'><Link to='/'>Home</Link></span>
         </Dropdown.Item>
         <Dropdown.Item>
-            <span className='DropText'><Link to="/signout" activeStyle={{ color: 'black' }}>Sign Out</Link></span>
+            <span className='DropText'><Link to="/signout" activeStyle={{ color: '#cb2d3e' }}>Sign Out</Link></span>
         </Dropdown.Item>
         <Dropdown.Item>
-            <span className='DropText'><Link to={`/dashboard/user/${props.user.userId}`} activeStyle={{ color: 'black' }}>Dashboard</Link></span>
+            <span className='DropText'><Link to={`/dashboard/user/${props.user.userId}`} activeStyle={{ color: '#cb2d3e' }}>Dashboard</Link></span>
         </Dropdown.Item>
     </Dropdown.Menu>
       : 
@@ -23,10 +23,10 @@ const DropDownMenu = props => (
             <span className='DropText'><Link to='/'>Home</Link></span>
         </Dropdown.Item>
         <Dropdown.Item>
-            <span className='DropText'><Link  to="/signin" activeStyle={{ color: 'black' }}>Sign in </Link></span>
+            <span className='DropText'><Link  to="/signin" activeStyle={{ color: '#cb2d3e' }}>Sign in </Link></span>
         </Dropdown.Item>
         <Dropdown.Item>
-            <span className='DropText'><Link  to="/signup" activeStyle={{ color: 'black' }}>Sign Up</Link></span>
+            <span className='DropText'><Link  to="/signup" activeStyle={{ color: '#cb2d3e' }}>Sign Up</Link></span>
         </Dropdown.Item>
     </Dropdown.Menu>
     }
